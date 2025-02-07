d32ede0317a7824c924df216eddad92f9af04893
MinhTrung187 <nguyenminhtrung18072004@gmail.com>
2025-02-07 22:15:53 +0700

    Initialized project, configured Tailwind CSS, and set up folder structure.

---
+++ app/App.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import "../global.css";
+
+
+export default function App() {
+  return (
+    <View className='flex-1 justify-center items-center bg-slate-400 ' >
+      <Text className="italic text-3xl font-semibold  " >Welcome to our Chill Bot app</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
